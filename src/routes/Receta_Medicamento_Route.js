25d121aa0c224b367e861791c32781a979e08a71
edgar <16030767@itcelaya.edu.mx>
2020-05-26 15:15:38 -0500

    Receta rutas

--- src/routes/Receta_Medicamento_Route.js
+++ src/routes/Receta_Medicamento_Route.js
@@ -6,11 +6,11 @@ const { Receta_Medicamento_Controller } = require('../controllers');
 // Rutas para el CRUD de las Recetas
 
 //Al hacer las peticiones a la ruta '/receta_mediccamentos' redirreciona al controlador de Recetas
-router.get('/r_m/',  Receta_Medicamento_Controller.getAll);
-router.get('/r_m/:id', Receta_Medicamento_Controller.getById);
-router.post('/r_m/', Receta_Medicamento_Controller.add);
-router.put('/r_m/', Receta_Medicamento_Controller.update);
-router.delete('/r_m/', Receta_Medicamento_Controller.delete);
+router.get('/',  Receta_Medicamento_Controller.getAll);
+router.get('/:id', Receta_Medicamento_Controller.getById);
+router.post('/', Receta_Medicamento_Controller.add);
+router.put('/', Receta_Medicamento_Controller.update);
+router.delete('/', Receta_Medicamento_Controller.delete);
 
 // Exporta el modulo de rutas
 module.exports = router;
--- src/routes/Receta_Route.js
+++ src/routes/Receta_Route.js
@@ -6,11 +6,11 @@ const { Receta_Controller } = require('../controllers');
 // Rutas para el CRUD de las Recetas
 
 //Al hacer las peticiones a la ruta '/receta' redirreciona al controlador de Recetas
-router.get('/receta/',  Receta_Controller.getAll);
-router.get('/receta/:id', Receta_Controller.getById);
-router.post('/receta/', Receta_Controller.add);
-router.put('/receta/', Receta_Controller.update);
-router.delete('/receta/', Receta_Controller.delete);
+router.get('/',  Receta_Controller.getAll);
+router.get('/:id', Receta_Controller.getById);
+router.post('/', Receta_Controller.add);
+router.put('/', Receta_Controller.update);
+router.delete('/', Receta_Controller.delete);
 
 // Exporta el modulo de rutas
 module.exports = router;
